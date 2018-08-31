ab2b7913f3ba5d69fc3b0bcfed000d57a4fc553e
trebortc <trebortc@hotmail.com>
2018-08-31 15:14:32 -0500

    Agregado perfil para secretaria

--- src/formularios/RegUsuario.java
+++ src/formularios/RegUsuario.java
@@ -195,7 +195,8 @@ DefaultComboBoxModel Ced_Emp;
 
         lblTipLogEmp.setText("Tipo de Usuario *");
 
-        cboTipLogEmp.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Seleccione", "Administrador", "Agente" }));
+        cboTipLogEmp.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Seleccione", "Administrador", "Agente", "Secretaria" }));
+        cboTipLogEmp.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         cboTipLogEmp.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 cboTipLogEmpActionPerformed(evt);
